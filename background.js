810429aab079f0da0308a8659378ae41886385a4
Anshika <anshikaatnitj14@gmamil.com>
2025-04-06 01:07:51 +0530

    Revert "chnages that are irrelevant"
    This reverts commit c2625b9f4ff79955fb11bdb4a63dc6e5341ef48d.

--- background.js
+++ background.js
@@ -111,7 +111,8 @@ for (let i = 0; i < textToSpeachSelected.length; i++) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         action: "text-to-speech-selected",
-        rate: rate.value,
+        rate: parseFloat(rate.value),
+        repeat: 2, // Request to repeat twice
       });
     });
   });
